86144a64a283b1be788798f18b6942bc5d6f651b
Lei Jie <839054164@qq.com>
2017-09-10 17:15:03 +0800

    income test first success

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 //new Date('2017-03-32').toString() "Invalid Date"
 
-var result = {
-	incomeSum: 0,
-	incomeA: 0,
-	incomeB: 0,
-	incomeC: 0,
-	incomeD: 0,
-};
+var result = {}; //保存存在的预定
 var reducedIncome;
-
-function bookAndCancel(str, result) {
-	//程序输出收入汇总
+var income = {
+	incomeSum: 0,
+	A: [],
+	B: [],
+	C: [],
+	D: [],
+}; //账单
+function bookAndCancel(str) {
+	//程status序输出收入汇总
 
 	if (/^\s$/.test(str)) {
 		var str = `收入汇总\n---
-		场地：A${result.incomeA}\n小计：${result.incomeA}\n
-		场地：B${result.incomeB}\n小计：${result.incomeB}\n
-		场地：C${result.incomeC}\n小计：${result.incomeC}\n
-		场地：D${result.incomeD}\n小计：${result.incomeD}\n
+		场地：A${income.A}\n小计：${income.A}\n
+		场地：B${income.B}\n小计：${income.B}\n
+		场地：C${income.C}\n小计：${income.C}\n
+		场地：D${income.D}\n小计：${income.D}\n
 		---
-		总计：${result.incomeSum}`;
-		console.log(str);
+		总计：${income.incomeSum}`;
+		console.log(income);
+		console.log(result);
 		return true;
 	}
 	var re = /^(\w+)\s+(\d{4}-\d{2}-\d{2})\s+(\d{2}:00)~(\d{2}:00)\s+(([ABCD]\s*$)|([ABCD]\sC\s*$))/;
@@ -45,28 +46,31 @@ function bookAndCancel(str, result) {
 		var incomeArr = computedIncome(weekday, startTime, endTime)
 
 		//预定
-		var info = matchArr.join("").replace(/\s+/, '');
+		var allInfo = matchArr.join("").replace(/\s+/, '');
+		var incomeInfo = matchArr
 		if (status.indexOf('C') < 1) {
-			var bookedStr = info; //去除多余空格
+			var bookedStr = allInfo; //去除多余空格
 			if (!(bookedStr in result)) {
 				result[bookedStr] = incomeArr;
-				result.incomeSum += incomeArr[0];
+				addIncome(bookedStr, incomeArr, income);
+
 			} else {
 				console.log("Error: the booking conflicts with existing bookings!")
 				return false;
 			}
 		} else { //取消
-			var cancelStr = info.slice(0, -1); //去掉C
+			var cancelStr = allInfo.slice(0, -1); //去掉C
 			if (cancelStr in result) {
 				delete result[cancelStr];
-				result.incomeSum -= incomeArr[1];
+				cancelIncome(cancelStr, incomeArr, income)
+
 			} else {
 				console.log('Error: the booking being cancelled does not exist!')
 				return false;
 			}
 		}
 		console.log("Success: the booking is accepted!")
-		console.log(result)
+			//console.log(result)
 		return true;
 	}
 }
@@ -105,14 +109,86 @@ function computedIncome(weekday, a, b) {
 		} else if (18 <= a && a < 22) {
 			if (b <= 22) income = (b - a) * 60;
 		}
-		reducedIncome = income / 4 * 3;
+		reducedIncome = income / 4;
 		incomeArr = incomeArr.concat(income, reducedIncome);
 	}
 	return incomeArr;
 
 }
+
+//预定时记录账单
+function addIncome(bookedStr, incomeArr, income) {
+	var status = bookedStr.slice(-1); //哪一组
+	var incomeInfo = bookedStr.slice()
+	switch (status) {
+		case 'A':
+			var obj = {};
+			obj[bookedStr] = incomeArr[0];
+			income.A.push(obj)
+			break;
+		case 'B':
+			var obj = {};
+			obj[bookedStr] = incomeArr[0];
+			income.B.push(obj)
+			break;
+		case 'C':
+			var obj = {};
+			obj[bookedStr] = incomeArr[0];
+			income.C.push(obj)
+			break;
+		case 'D':
+			var obj = {};
+			obj[bookedStr] = incomeArr[0];
+			income.D.push(obj)
+			break;
+	}
+	income.incomeSum += incomeArr[0];
+}
+
+//取消预定后的账单
+function cancelIncome(cancelStr, incomeArr, income) {
+	var status = cancelStr.slice(-1); //哪一组
+	deleteIncome(status, cancelStr, income); //删除预定时的原账单
+	switch (status) {
+		case 'A':
+			var obj = {};
+			obj[cancelStr] = incomeArr[1];
+			income.A.push(obj)
+			break;
+		case 'B':
+			var obj = {};
+			obj[cancelStr] = incomeArr[1];
+			income.B.push(obj)
+			break;
+		case 'C':
+			var obj = {};
+			obj[cancelStr] = incomeArr[1];
+			income.C.push(obj)
+			break;
+		case 'D':
+			var obj = {};
+			obj[cancelStr] = incomeArr[1];
+			income.D.push(obj)
+			break;
+	}
+	income.incomeSum -= incomeArr[0];
+	income.incomeSum += incomeArr[1];
+}
+
+function deleteIncome(status, cancelStr, income) {
+	var arr = income[status];
+	var len = arr.length;
+	for (var i = 0; i < len; i++) {
+		if (cancelStr in arr[i]) {
+			arr.splice(i, 1);
+			break;
+		}
+	}
+}
 module.exports = {
 	bookAndCancel,
 	result,
+	income,
 	computedIncome,
+	deleteIncome
 };
--- app.test.js
+++ app.test.js
@@ -4,7 +4,9 @@ import {
 import {
 	bookAndCancel,
 	result,
-	computedIncome
+	computedIncome,
+	deleteIncome,
+	income
 } from './app.js';
 // var add = require('./app.js');
 // var expect = require('chai').expect;
@@ -20,38 +22,38 @@ describe('正则匹配的初步检查', function() {
 	// 	expect(bookAndCancel('U123 2016-06-02 20:00~22:00 A C')).to.be.true;
 	// });
 	it('U123 2016-06-02 20:00~22:00 A 应该通过', function() {
-		expect(bookAndCancel('U123 2016-06-02 20:00~22:00 A', result)).to.be.true;
+		expect(bookAndCancel('U123 2016-06-02 20:00~22:00 A')).to.be.true;
 	});
 	it('U123 2016-06-02 20:00~22:00 不应该通过', function() {
-		expect(bookAndCancel('U123 2016-06-02 20:00~22:00', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-06-02 20:00~22:00')).to.be.false;
 	});
 	it('U123 2016-12-02 20:00~22:00 A B不应该通过', function() {
-		expect(bookAndCancel('U123 2016-12-02 20:00~22:00 A B', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-12-02 20:00~22:00 A B')).to.be.false;
 	});
 	it('U123 2016-12-02 20:00~22:00 B 应该通过', function() {
-		expect(bookAndCancel('U123 2016-12-02 20:00~22:00 B', result)).to.be.true;
+		expect(bookAndCancel('U123 2016-12-02 20:00~22:00 B')).to.be.true;
 	});
 	it(' 2016-12-02 20:00~22:00 B 不应该通过', function() {
-		expect(bookAndCancel(' 2016-12-02 20:00~22:00 B', result)).to.be.false;
+		expect(bookAndCancel(' 2016-12-02 20:00~22:00 B')).to.be.false;
 	});
 
 });
 
 describe("日期的合法化检查", function() {
 	it('U123 2016-13-02 20:00~22:00 A 月份不对', function() {
-		expect(bookAndCancel('U123 2016-13-02 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-13-02 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-06-2 20:00~22:00 A 日子不对', function() {
-		expect(bookAndCancel('U123 2016-06-2 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-06-2 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-00-02 20:00~22:00 A 月份不对', function() {
-		expect(bookAndCancel('U123 2016-00-02 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-00-02 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-2-31 20:00~22:00 A 日子超了不对', function() {
-		expect(bookAndCancel('U123 2016-2-31 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-2-31 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-2-2 20:00~22:00 A 月份日子不对', function() {
-		expect(bookAndCancel('U123 2016-2-2 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-2-2 20:00~22:00 A')).to.be.false;
 	});
 	/*下面对特殊月份进行检查：
 		new Date('2015-02-28')
@@ -85,46 +87,46 @@ describe("日期的合法化检查", function() {
 
 describe("时间的合法化检查", function() {
 	it('U123 2016-13-02 20:00~22:00 A 月份不对', function() {
-		expect(bookAndCancel('U123 2016-13-02 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-13-02 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-06-2 20:00~22:00 A 日子不对', function() {
-		expect(bookAndCancel('U123 2016-06-2 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-06-2 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-00-02 20:00~22:00 A 月份不对', function() {
-		expect(bookAndCancel('U123 2016-00-02 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-00-02 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-2-31 20:00~22:00 A 日子超了不对', function() {
-		expect(bookAndCancel('U123 2016-2-31 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-2-31 20:00~22:00 A')).to.be.false;
 	});
 	it('U123 2016-2-2 20:00~22:00 A 月份日子不对', function() {
-		expect(bookAndCancel('U123 2016-2-2 20:00~22:00 A', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-2-2 20:00~22:00 A')).to.be.false;
 	});
 })
 
 describe("预定和取消功能检测", function() {
 	it('U123 2016-06-03 20:00~22:00 A 预定成功', function() {
-		expect(bookAndCancel('U123 2016-06-03 20:00~22:00 A', result)).to.be.true;
+		expect(bookAndCancel('U123 2016-06-03 20:00~22:00 A')).to.be.true;
 	});
 	it('U123 2016-06-02 20:00~22:00 A C 取消成功', function() {
-		expect(bookAndCancel('U123 2016-06-02 20:00~22:00 A C', result)).to.be.true;
+		expect(bookAndCancel('U123 2016-06-02 20:00~22:00 A C')).to.be.true;
 	});
 	it('U123 2016-06-02 22:00~22:00 时间错误预定失败', function() {
-		expect(bookAndCancel('U123 2016-06-02 22:00~22:00', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-06-02 22:00~22:00')).to.be.false;
 	});
 	it('U123 2016-12-01 20:00~22:00 B 预定成功', function() {
-		expect(bookAndCancel('U123 2016-12-01 20:00~22:00 B', result)).to.be.true;
+		expect(bookAndCancel('U123 2016-12-01 20:00~22:00 B')).to.be.true;
 	});
 	it('U123 2016-12-02 20:00~22:00 A C 没有预定取消失败', function() {
-		expect(bookAndCancel('U123 2016-12-02 20:00~22:00 A C', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-12-02 20:00~22:00 A C')).to.be.false;
 	});
 	it('U123 2016-12-01 20:00~22:00 B 已经被预定预定失败', function() {
-		expect(bookAndCancel('U123 2016-12-01 20:00~22:00 B', result)).to.be.false;
+		expect(bookAndCancel('U123 2016-12-01 20:00~22:00 B')).to.be.false;
 	});
 })
 
 describe("收入汇总测试", function() {
 	it('收入汇总测试，输入一个空格 ', function() { //一个空格
-		expect(bookAndCancel(' ', result)).to.be.true;
+		expect(bookAndCancel(' ')).to.be.true;
 	});
 
 })
